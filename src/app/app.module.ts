99d4b25577c2d04e12226e34ca43cbc8f809accf
daidh <duonghuudai.id002@gmail.com>
2023-06-11 20:28:38 +0700

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+const environment = {
+  production: true
+}
+
+const router = [{
+  path: '',
+  component: HomeComponent
+},{
+  path: 'about',
+  component: AboutComponent
+}]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(router),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
